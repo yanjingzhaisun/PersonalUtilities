7bdf4a685c446ba94450dbf2b24219d93fc7436a
Euphonux <zz1322@nyu.edu>
2016-10-04 00:55:53 -0400

    first commit

---
+++ pushthesisdoc.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+import os
+import glob
+import shutil
+
+# get a list of all txt files
+rmline = "rm -rf /Users/zijianzhou/Documents/Python/utils/docs/html"
+os.system(rmline)
+os.system("echo \"finished removing docs\"")
+os.system("doxygen /Users/zijianzhou/Documents/Python/utils/docs/Doxyfile")
+os.system("echo \"finished generating docs\"")
+# not sure about the address
+copyline = "sshpass -p raspberrypi scp /Users/zijianzhou/Documents/Python/utils/docs pi@172.168.22.31:~/usbdrv/monsterfarmdocs/"
+os.system(copyline)
+os.system("echo \"finish copying docs\"")
+import paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+hostname = '172.22.31.168'
+username = 'pi'
+# 私钥的密码，没有则不需要传递password参数
+password = 'raspberry'
+ssh.connect(hostname, username = username, password = password)
+stdin, stdout, stderr = ssh.exec_command('sudo reboot')
+
+ssh.close()
